92ddc0044867d3b69fdf5a6f74658e7a86625a02
Haile-08 <haile4cmd@gmail.com>
2023-06-01 01:51:44 -0700

    Write a function that checks if a given node is a root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - tree is root
+ * @node: pointer to the node to check
+ * Description: checks if a given node is a root
+ * Return: 1 if node is a root, otherwise 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node != NULL && node->parent == NULL)
+		return (1);
+	return(0);
+}
