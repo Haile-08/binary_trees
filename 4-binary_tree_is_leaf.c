d6a1354fe93ba349e6625f49e6e18c98d0a38b72
Haile-08 <haile4cmd@gmail.com>
2023-06-01 01:37:52 -0700

    Refactor

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,9 +10,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-	if (node->left != NULL || node->right != NULL)
-		return (0);
-	return (1);
+	int n = 0;
+
+	if (node != NULL)
+		n = ((node->left == NULL) && (node->right == NULL) ? 1 : 0);
+	return (0);
 }
